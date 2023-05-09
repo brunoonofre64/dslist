c2cd9eeb8bb2851af64e2ce3b47ef0af9598dd65
onofre <brunoonofre64@gmail.com>
2023-05-09 15:09:40 -0300

    criada a entidade game e script sql inicial no import.sql

--- src/main/java/io/github/brunoonofre64/dslist/entities/GameEntity.java
+++ src/main/java/io/github/brunoonofre64/dslist/entities/GameEntity.java
@@ -17,11 +17,14 @@ public class GameEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private Double score;
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 }
