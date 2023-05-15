81069fec27a87183ef4053ebd5c7f06462e95fcb
onofre <brunoonofre64@gmail.com>
2023-05-14 21:06:07 -0300

    implementando consulta de games por genero

--- src/main/java/io/github/brunoonofre64/dslist/infrastructure/jpa/repositories/GameRepository.java
+++ src/main/java/io/github/brunoonofre64/dslist/infrastructure/jpa/repositories/GameRepository.java
@@ -2,8 +2,18 @@ package io.github.brunoonofre64.dslist.infrastructure.jpa.repositories;
 
 import io.github.brunoonofre64.dslist.domain.entities.GameEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GameRepository extends JpaRepository<GameEntity, Long> {
+
+    @Query("SELECT g FROM GameEntity g JOIN BelongingEntity b " +
+            "ON g.id = b.belongingPK.game.id WHERE b.belongingPK.gameList.id = :listId " +
+            "ORDER BY b.position")
+    List<GameEntity> searchByList(Long listId);
+
+
 }
