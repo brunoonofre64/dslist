4a301422e1e2bcff152079bc7e93829eac9cde50
onofre <brunoonofre64@gmail.com>
2023-05-14 21:06:32 -0300

    implementando endpoint de consulta de lista de games por genero

--- src/main/java/io/github/brunoonofre64/dslist/api/v1/controller/GameListController.java
+++ src/main/java/io/github/brunoonofre64/dslist/api/v1/controller/GameListController.java
@@ -1,13 +1,12 @@
 package io.github.brunoonofre64.dslist.api.v1.controller;
 
 import io.github.brunoonofre64.dslist.domain.dto.GameListDTO;
+import io.github.brunoonofre64.dslist.domain.dto.GameMinDTO;
 import io.github.brunoonofre64.dslist.infrastructure.service.GameListService;
+import io.github.brunoonofre64.dslist.infrastructure.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +16,17 @@ import java.util.List;
 public class GameListController {
 
     private final GameListService gameListService;
+    private final GameService gameService;
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
     public List<GameListDTO> findAll() {
         return gameListService.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{listId}/games")
+    public List<GameMinDTO> findAllByList(@PathVariable(name = "listId") Long listId) {
+        return gameService.findAllByList(listId);
     }
 }
